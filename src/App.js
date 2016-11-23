595d0d89d10cf69514810d656afae02c97fe5705
thedamfr <dam.cavailles@gmail.com>
2016-11-23 07:57:24 +0100

    First Navigation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import {
   AppRegistry,
-  StyleSheet,
   Text,
-  View
+  View,
+  Navigator,
+  Linking
 } from 'react-native';
 import React, { Component } from 'react'
 import { Provider } from 'react-redux'
 import store from './redux/store.js'
+import HomeScene from './scenes/HomeScene.js';
+import NextScene from './scenes/NextScene.js';
+import PromoScene from './scenes/PromoScene.js';
 
 
 import KerKerContainer from './components/KerKerContainer.js'
@@ -18,38 +22,44 @@ class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <View style={styles.container}>
-          <Text style={styles.welcome}>
-            Welcome to React Native!
-          </Text>
-          <KerKerContainer/>
-          <Text style={styles.instructions}>
-            Press Cmd+R to reload,{'\n'}
-            Cmd+D or shake for dev menu
-          </Text>
-        </View>
+        <Navigator
+          initialRoute={{ title: 'Welcome', index: 0 }}
+          renderScene={(route, navigator) => {
+
+            switch(route.title) {
+              case 'Welcome':
+                return (<HomeScene
+                  onForward={
+                    () => {
+                      navigator.push({
+                        title: 'Next Scene',
+                        index: 1
+                      });
+                    }
+                  }
+                  />);
+                break;
+              case 'Next Scene':
+                return (<NextScene
+                  onForward={
+                    () => {
+                      navigator.push({
+                        title: 'Promo Scene',
+                        index: 1
+                      });
+                    }
+                  }
+                  />);
+                break;
+              case 'Promo Scene':
+                return (<PromoScene />);
+                break;
+              }
+          }}
+        />
       </Provider>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 export default App
