187c760c734e024e5cab38fe14364183bdbdfbbf
thedamfr <dam.cavailles@gmail.com>
2017-02-16 07:57:48 +0100

    :confetti_ball: working ios-first demo

--- ios-first/ios/iOSFirst/ViewController.swift
+++ ios-first/ios/iOSFirst/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import React
+
 
 class ViewController: UIViewController {
 
@@ -20,6 +22,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func kerkerButtonTapped(sender : UIButton) {
+        NSLog("Hello")
+        let jsCodeLocation = URL(string: "http://localhost:8081/index.ios.bundle?platform=ios")
+        let mockData:NSDictionary = ["scores":
+            [
+                ["name":"Alex", "value":"42"],
+                ["name":"Joel", "value":"10"]
+            ]
+        ]
+        
+        let rootView = RCTRootView(
+            bundleURL: jsCodeLocation,
+            moduleName: "KerKer",
+            initialProperties: mockData as [NSObject : AnyObject],
+            launchOptions: nil
+        )
+        let vc = UIViewController()
+        vc.view = rootView
+        self.present(vc, animated: true, completion: nil)
+    }
 
 }
 
