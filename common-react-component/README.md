6935ab9535a132fc9b220479c81ab33ba51ab23a
thedamfr <dam.cavailles@gmail.com>
2017-04-18 11:43:48 +0200

    common component

---
+++ common-react-component/README.md
@@ -0,0 +1,3 @@
+## Why
+
+This react native script is used both by android-first and iOS-first.
---
+++ common-react-component/src/App.js
@@ -0,0 +1,58 @@
+import {
+  Text,
+  View,
+} from 'react-native';
+
+import React, { Component } from 'react'
+
+class App extends Component
+
+  constructor() {
+
+  }
+
+  componentWillUpdate() {
+
+  }
+
+  componentDidUpdate() {
+
+  }
+
+  shouldCompomentUpdate() {
+
+  }
+
+  componentWillMount() {
+
+  }
+
+  componentDidMount() {
+
+  }
+
+  componentWillUnmount() {
+
+  }
+
+  componentWillReceiveProps() {
+
+  }
+
+
+
+  render() {
+    return {
+      <div>
+        <div>
+          <div>Ceci est une div</div>
+          <div>
+            <p>Ceci est un paragraphe</p>
+            <h1>Oh ! Un Header</h1>
+          </div>
+        </div>
+      </div>
+    }
+  }
+
+}
